eceb7ff1daba1b16755e880d59c2ed76dd034280
lisandrocoronel3444 <lisandrojulian2004@gmail.com>
2024-03-26 16:26:06 -0300

    hice una prueba del gitignore y avance con el punto 3

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 main(){
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+    int num = 5;
+    int * pPunt = &num;
+    printf("El contenido del puntero es %d", *(pPunt));
+    printf("Direccion almacenada %d", pPunt);
+    
 
 
 
