b79faf4a05b544ab303c9175a872cfd922701cb4
Lisandro Coronel <lisandrojulian2004@gmail.com>
2024-04-10 12:28:57 -0300

    Realice el punto 3 agregando el puntero y todo lo pedido

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -4,8 +4,11 @@ main(){
     printf("Hola mundo\n");
     int num = 5;
     int * pPunt = &num;
-    printf("El contenido del puntero es %d", *(pPunt));
-    printf("Direccion almacenada %d", pPunt);
+    printf("El contenido del puntero es %d\n", *(pPunt));
+    printf("Direccion almacenada %d\n", pPunt);
+    printf("Direccion de memoria de la variable: %d\n", &num);
+    printf("Direccion de memoria de puntero: %d\n", &pPunt);
+    printf("Tamanio de la variable: %d\n", sizeof(num));
     
 
 
